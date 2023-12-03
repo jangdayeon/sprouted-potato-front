d3a9db26d8ad737c8b12fd3e76305f7bdef99916
jangdayeon <hyejmh@naver.com>
2023-12-03 18:04:12 +0900

    complete the CSS of bookList

--- src/components/main/EmojiReview.jsx
+++ src/components/main/EmojiReview.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 import ListSet from '../main/ListSet'
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRobot, faThumbsUp, faComments, faBookBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const EmojiReviewOutDiv = styled.div`
     width: 100%;
-    height: 500px;
     background-color: white;
+    padding-bottom : 70px;
 `
-
 const ImageOutDiv = styled.div`
     width: 100%;
     height : default;
@@ -38,14 +40,19 @@ cursor: pointer;
 const Listname = styled.div`
     font-size : 1.3em;
     font-family: "Pretendard-Regular";
-    padding : 0.2em 0em 0.4em 0.9em;
+    padding : 0.2em 0em 0.8em 1.8em;
+    font-weight: 700;
 `
 const ListBr = styled.hr`
-    background-color : gray;
+    background-color : #CECECE ;
     border : 0;
     width : 95%;
-    height : 0.2em;
+    height : 0.1em;
     margin : 1.5em auto;
+`
+const ListDiv = styled.div`
+    width : 95%;
+    margin : 0 auto;
 `
 function EmojiReview() {
     const [radioClicked, setRadioClicked] = useState("😄");
@@ -68,6 +75,105 @@ function EmojiReview() {
             case "😍" : setRadioTextClicked("반했어요");break;
         }
     },[radioClicked]);
+
+    //책 리스트 받아오기(api연결)
+
+    const [bookList1, setBookList1] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const url = 'http://localhost:8080/bestseller';
+          const response = await axios.get(url);
+          const responseData = response.data.data;
+          setBookList1(responseData);
+          console.log(responseData);
+  
+        } catch(error) {
+          console.log(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
+    const [bookList2, setBookList2] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const url = 'http://localhost:8080/bestseller';
+          const response = await axios.get(url);
+          const responseData = response.data.data;
+          setBookList2(responseData.concat(responseData));
+          console.log(responseData);
+  
+        } catch(error) {
+          console.log(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
+    const [bookList3, setBookList3] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+        //   const url = 'http://localhost:8080/bestseller';
+        //   const response = await axios.get(url);
+        //   const responseData = response.data.data;
+        //   setBookList3(responseData);
+        //   console.log(responseData);
+  
+        } catch(error) {
+          console.log(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
+    const [bookList4, setBookList4] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+        //   const url = 'http://localhost:8080/bestseller';
+        //   const response = await axios.get(url);
+        //   const responseData = response.data.data;
+        //   setBookList4(responseData);
+        //   console.log(responseData);
+  
+        } catch(error) {
+          console.log(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
+    const [bookList5, setBookList5] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+        //   const url = 'http://localhost:8080/bestseller';
+        //   const response = await axios.get(url);
+        //   const responseData = response.data.data;
+        //   setBookList5(responseData);
+        //   console.log(responseData);
+  
+        } catch(error) {
+          console.log(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+
+
 
     return (
         <EmojiReviewOutDiv>
@@ -103,8 +209,21 @@ function EmojiReview() {
                         </ImagePreview>
             </ImageOutDiv>
             <Listname>{radioClicked} '{radioTextClicked}'가 많은 순</Listname>
-            <ListSet/>
+            <ListDiv>
+            <ListSet bookList={bookList2}/>
+            </ListDiv>
             <ListBr/>
+            <Listname><FontAwesomeIcon icon={faRobot} /> AI가 분석한 긍정적인 후기 순</Listname>
+            <ListSet bookList={bookList2}/>
+            <ListBr/>
+            <Listname><FontAwesomeIcon icon={faThumbsUp} /> 긍정적인 후기 순</Listname>
+            <ListSet bookList={bookList3}/>
+            <ListBr/>
+            <Listname><FontAwesomeIcon icon={faComments} /> 리뷰 많은 많은 순</Listname>
+            <ListSet bookList={bookList4}/>
+            <ListBr/>
+            <Listname><FontAwesomeIcon icon={faBookBookmark} /> 알라딘 베스트셀러</Listname>
+            <ListSet bookList={bookList5}/>
         </EmojiReviewOutDiv>
     )
 }
